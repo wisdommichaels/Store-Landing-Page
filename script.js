60a1129dc9eef3510d38c178c377f4af73db54f4
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-03 23:21:23 +0100

    added js file

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Here you can add your form validation and authentication logic
